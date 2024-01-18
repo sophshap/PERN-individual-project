b953ce22875fa5bde24f2fa5e14e825bcc2120b2
sophshap <sophieashapiro@gmail.com>
2024-01-18 12:31:42 -0500

    new scripts

--- server/db/client.js
+++ server/db/client.js
@@ -1,5 +1,6 @@
 const { Client } = require('pg')
 const dbName = "drinksrecipes"
-const client = new Client(`postgres://localhost:54321/${dbName}`)
+const host = process.env.DB_URL || `postgres://localhost:54321/${dbName}`
+const client = new Client(host)
 
 module.exports = client
